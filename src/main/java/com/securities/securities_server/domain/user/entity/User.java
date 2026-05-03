8e34fcb0135e87762ad9f81ef5341053f7034d6e
Kiju Jeong <kijujeong.dev@gmail.com>
2026-05-03 17:03:06 +0900

    [#3] feat: User Entity 구현

---
+++ src/main/java/com/securities/securities_server/domain/user/entity/User.java
@@ -0,0 +1,39 @@
+package com.securities.securities_server.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@Getter
+public class User {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 10)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static User register(String name, String email, String password) {
+        return new User(name, email, password);
+    }
+}
