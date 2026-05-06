09074b1f4f5ef55260ad3b2f2b46a41e7328b7a0
Kiju Jeong <kijujeong.dev@gmail.com>
2026-05-06 16:22:46 +0900

    [#3] feat: 예약어 이슈로 테이블명 users로 수정

--- src/main/java/com/securities/securities_server/domain/user/entity/User.java
+++ src/main/java/com/securities/securities_server/domain/user/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
+@Table(name = "users")
 @NoArgsConstructor(access = PROTECTED)
 @Getter
 public class User {
