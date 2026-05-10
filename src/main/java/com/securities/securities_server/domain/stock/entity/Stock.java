bca4cf2327fad7ac3b4581eeaa34f3f9c9b5338c
Kiju Jeong <kijujeong.dev@gmail.com>
2026-05-10 16:42:27 +0900

    [#7] feat: 종목 계좌 기능 구현을 위한 엔티티 구현

--- src/main/java/com/securities/securities_server/domain/stock/entity/Stock.java
+++ src/main/java/com/securities/securities_server/domain/stock/entity/Stock.java
@@ -29,9 +29,4 @@ public class Stock extends BaseEntity {
 
     @Column(nullable = false, unique = true, length = 6)
     private String code;
-
-    public Stock(String name, String code) {
-        this.name = name;
-        this.code = code;
-    }
 }
